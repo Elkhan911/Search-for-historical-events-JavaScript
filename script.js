9c30744bffbf47c448f9d6719c74379939c25949
Elkhan911 <955pro@mail.ru>
2022-08-23 16:40:16 +0500

    dealed with conditions for input1

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const span1 = document.querySelector("#_span1");
 
 input1.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    span1.textContent = input1.value;
-    input1.value = "";
+    if (input1.value > 2000 && input1.value < 2022) {
+      span1.textContent = "";
+      span1.textContent = input1.value;
+      input1.value = "";
+    } else {
+      span1.textContent =
+        input1.value + " -  необходимо ввести год с 2000 до 2022";
+    }
   }
 });
